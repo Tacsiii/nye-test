b1965f09b58d4f2e6e37a9970c94314d076c044c
Farkas Máté <tacsika001@gmail.com>
2022-09-25 15:17:42 +0200

    child brachre committtt

--- src/Dalile.java
+++ src/Dalile.java
@@ -2,6 +2,6 @@ public class Dalile {
     public static void main(String[] args) {
         System.out.println("animal!!");
         System.out.println("commitoltam valamit!!");
-
+        System.out.println("még valamit!!");
     }
 }
