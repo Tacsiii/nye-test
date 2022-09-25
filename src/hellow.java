d219af19521f26fa5fcd85b1a4e5c19a7569457d
Farkas Máté <tacsika001@gmail.com>
2022-09-25 14:24:14 +0200

    chaged to second Commit

--- src/hellow.java
+++ src/hellow.java
@@ -1,7 +1,7 @@
 public class hellow {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!");
 
         System.out.println("another one");
 
