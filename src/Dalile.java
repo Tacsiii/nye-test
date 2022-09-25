f29dc7fb3102b81906f13caca9723e4cc7c66dc1
Farkas Máté <tacsika001@gmail.com>
2022-09-25 15:10:41 +0200

    forth commit

---
+++ src/Dalile.java
@@ -0,0 +1,9 @@
+public class Dalile {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+
+        System.out.println("another one");
+
+        System.out.println("another another one");
+    }
+}
